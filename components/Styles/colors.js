3dfdc15c23ce73af137d1e33a8aa04df55492d7b
Andre Almeida <andre-silva78@hotmail.com>
2022-11-02 17:37:19 -0300

    Create style helpers

---
+++ components/Styles/colors.js
@@ -0,0 +1,8 @@
+const color = {
+  bg50positive: '#EBFAF4',
+  bg50negative: '#FCEDED',
+  content800positive: '#214739',
+  content800negative: '#5C3030',
+};
+
+export default color;
---
+++ components/Styles/fonts.js
@@ -0,0 +1,19 @@
+export const xxxs = `
+  font-size: 10px;
+  line-height: 12px;
+`;
+
+export const xxs = `
+  font-size: 12px;
+  line-height: 16px;
+`;
+
+export const sm = `
+  font-size: 16px;
+  line-height: 24px;
+` 
+
+export const md = `
+  font-size: 20px;
+  line-height: 28px;
+`
